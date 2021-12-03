80550da68f1fe93af77b0357eecf9e7c79560635
RazvanDragos <razvan_dragos09@yahoo.com>
2021-12-03 19:42:58 +0200

    Created react app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="appContainer">
+        <div id="A" className="containerItem">A</div>
+        <div id="B" className="containerItem">B</div>
+        <div id="C" className="containerItem">C</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/common/ui/Box/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./styles.css";
+
+const Box = () => {
+  return <div id="box">Box</div>
+}
+
+export default Box;
